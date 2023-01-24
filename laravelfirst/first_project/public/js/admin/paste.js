c414a70914f3c1159fa47b08eff4365a2dd7ef4b
RomanPershi <96135424+RomanPershi@users.noreply.github.com>
2023-01-24 19:21:38 +0400

    Update paste.js

--- laravelfirst/first_project/public/js/admin/paste.js
+++ laravelfirst/first_project/public/js/admin/paste.js
@@ -1,11 +1,13 @@
 const form = document.getElementById("new_document_attachment");
 const fileInput = document.getElementById("document_attachment_doc");
 window.addEventListener('paste', e => {
-    fileInput.files = e.clipboardData.files;
-    const [file] = document_attachment_doc.files
-    if (file) {
-        blah.src = URL.createObjectURL(file);
-        $('#blah').css('display', 'block');
+    if (e.clipboardData.files.length == 1) {
+        fileInput.files = e.clipboardData.files;
+        const [file] = document_attachment_doc.files
+        if (file) {
+            blah.src = URL.createObjectURL(file);
+            $('#blah').css('display', 'block');
+        }
     }
 });
 document_attachment_doc.onchange = evt => {
